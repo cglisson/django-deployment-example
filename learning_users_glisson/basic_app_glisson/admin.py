1c2447a39159f337d47f3a009fcc86193e4d52ab
Casey Glisson <cglisson@gmail.com>
2017-11-24 16:13:01 -0500

    'first'

---
+++ learning_users_glisson/basic_app_glisson/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from basic_app_glisson.models import UserProfileInfo
+
+# Register your models here.
+admin.site.register(UserProfileInfo)
---
+++ learning_users_glisson/basic_app_glisson/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BasicAppGlissonConfig(AppConfig):
+    name = 'basic_app_glisson'
