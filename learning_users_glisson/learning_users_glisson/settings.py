3817aad595b89d0b4943fecff139597ed286c2f7
Casey Glisson <cglisson@gmail.com>
2017-11-24 19:56:28 -0500

    'first'

--- learning_users_glisson/learning_users_glisson/settings.py
+++ learning_users_glisson/learning_users_glisson/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '$m@nx-qr#03&&*(zghy^iatuib)o9&n)i=@-07b6&(m31i5m&*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cglisson.pythonanywhere.com']
 
 
 # Application definition
